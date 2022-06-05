64a312484ae6bce7606246c73c592d6ee9329b5f
Designa <designa@Designas-MacBook-Pro.local>
2022-06-05 13:49:01 -0500

    Creacion del backend con mongoose

---
+++ api/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Database connected!");
+}).catch(err => {
+	console.log(err)
+    console.error("App starting error:", err.message);
+});
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//Routes
+app.use('/api',require('./routes/api'));
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
